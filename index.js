94e4a612aaf2989a79b098a6151f2ab0322a35f4
mysticankush <68521128+mysticankush@users.noreply.github.com>
2021-11-11 15:09:59 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,58 @@
+var total_buttons=document.getElementsByClassName('drum').length
+  document.addEventListener("keypress",function(event) {
+  var buttonClicked=event.key;
+  makesound(buttonClicked);
+  buttonAnimation(buttonClicked);
+});
+for(var i=0;i<total_buttons;i++)
+{
+  document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    var buttonClicked = this.innerHTML;
+    makesound(buttonClicked);
+    buttonAnimation(buttonClicked);
+  })
+}
+function makesound(key)
+{
+  switch (key)
+  {
+    case 'w':
+        var tom1=new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+    case 'a':
+        var tom2=new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+    case 's':
+        var tom3=new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+    case 'd':
+        var tom4=new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+    case 'j':
+        var snare=new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+    case 'k':
+        var crash=new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+    case 'l':
+        var kick=new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+
+    default :Alert("You clicked an invalid number that is : " + buttonClicked);
+  }
+}
+function buttonAnimation(currentKey)
+{
+    var activeButton=document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+      activeButton.classList.remove("pressed");
+    },100);
+}
